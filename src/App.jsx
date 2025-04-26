e43b71902590c345c1675c4d44a1772275b6ab61
Aman Mishra <amanmishra.5272@gmail.com>
2025-04-26 15:02:43 +0530

    Refactor App component for improved readability and added ScrollToTop functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Issues from './Pages/Issues';
-import View from './Pages/View';
+import Issues from "./Pages/Issues";
+import View from "./Pages/View";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  }
   return (
     <div>
+      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Issues/>} />
+        <Route path="/" element={<Issues />} />
         <Route path="/volume-13" element={<Issues />} />
         <Route path="/volume-13/issue-4-april-2025" element={<Issues />} />
-        <Route path="/volume-13/issue-4-april-2025/how-facial-recognition-systems-are-used-for-identification-of-missing-person" element={<View />} />
+        <Route
+          path="/volume-13/issue-4-april-2025/how-facial-recognition-systems-are-used-for-identification-of-missing-person"
+          element={<View />}
+        />
       </Routes>
       {/* <View/> */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
